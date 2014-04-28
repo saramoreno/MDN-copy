389aefa006361e112fcd24c2029f37e0a7c79d95
Sara Moreno <sara.morenoj@gmail.com>
2014-04-28 18:13:35 +0200

    included textures

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,7 +1,17 @@
 $(function () {
-  $(".drop-bar i").on("click", abreMenu);
+  $("a.drop-bar").on("click", abreMenu);
 
   function abreMenu() {
     $(".dropdown-menu").slideToggle();
+  }
+});
+
+//aside desplegable
+
+$(function () {
+  $(".drop-bar-aside a").on("click", abreMenu);
+
+  function abreMenu() {
+    $(".drop-content li").slideToggle();
   }
 });
